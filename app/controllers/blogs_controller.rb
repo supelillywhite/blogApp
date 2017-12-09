51251a83346ae740d7b8fd62472f8518c60f12ff
Jerel Lillywhite <supelillywhite@Jerels-MBP.domain>
2017-12-09 14:54:12 -0700

    made index, new and create actions for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,26 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(params.require(:blog).permit(:title, 
+                                                  :date, 
+                                                  :author, 
+                                                  :subject, 
+                                                  :body))
+
+    respond_to do |format|
+      if @blog.save
+        format.html { redirect_to blogs_path, notice: 'Your blog post is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
